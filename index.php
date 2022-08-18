9557f832dfdb86f4f37c8f90d73c262e5d087076
Rodrigo-M-Brazao <rodrigombrazao@hotmail.com>
2022-08-17 22:08:07 -0300

    Primeiro pull

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+    <?php
+        // Primeiro exercicio de PHP
+
+        #Cometário de linha também
+
+        /*
+            Comentário
+                de
+              Bloco
+        */
+        
+        
+        $nome = 'Rodrigo Milani';
+        echo "Olá mundo, meu nome é $nome!";
+    ?>
+</html>
